41152086b5e8333b6deda62b43cc4bcee21426f4
Andre Furlan <furlan.andre91@gmail.com>
2025-04-22 22:40:34 -0400

    feat: adicionado documentação na controller de tarefa, com seus respectivos contratos dos serviços

--- Tarefas.Api/Tarefas/TarefaController.cs
+++ Tarefas.Api/Tarefas/TarefaController.cs
@@ -1,8 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
+using Tarefas.Aplicacao.Tarefas.Dtos;
 using Tarefas.Aplicacao.Tarefas.Interfaces;
-using Tarefas.Dominio.Tarefas;
 
-namespace Tarefas.Api;
+namespace Tarefas.Api.Tarefas;
 
 [ApiController]
 [Route("api/tasks/")]
@@ -11,46 +11,77 @@ namespace Tarefas.Api;
 public class TarefaController : ControllerBase
 {
     private readonly IArmazenadorTarefa _armazenadorTarefa;
+    private readonly IConsultaTarefa _consultaTarefa;
+    private readonly IConcluidorTarefa _concluidorTarefa;
+    private readonly IRemovedorTarefa _removedorTarefa;
 
-    public TarefaController(IArmazenadorTarefa armazenadorTarefa)
+    public TarefaController(
+        IArmazenadorTarefa armazenadorTarefa,
+        IConsultaTarefa consultaTarefa,
+        IConcluidorTarefa concluidorTarefa,
+        IRemovedorTarefa removedorTarefa)
     {
         _armazenadorTarefa = armazenadorTarefa;
+        _consultaTarefa = consultaTarefa;
+        _concluidorTarefa = concluidorTarefa;
+        _removedorTarefa = removedorTarefa;
     }
 
     /// <summary>
     /// Cria uma tarefa nova para um user Id
     /// </summary>
-    /// <param name="">A tarefa para requisitada</param>
-    /// <returns>Os detalhes da tarefea criada</returns>
+    /// <param name="armazenadorTarefaDto">Dados para criação de uma nova tarefa vinculada a um usuário</param>
+    /// <returns>Os detalhes da tarefa criada</returns>
     [HttpPost]
-    [ProducesResponseType(typeof(IActionResult), StatusCodes.Status201Created)]
+    [ProducesResponseType(typeof(TarefaDto), StatusCodes.Status201Created)]
     [ProducesResponseType(typeof(IActionResult), StatusCodes.Status400BadRequest)]
-    public async Task<IActionResult> CriarTarefa()
+    public async Task<IActionResult> CriarTarefa([FromBody] ArmazenadorTarefaDto armazenadorTarefaDto)
     {
-        // var tarefinha = new Tarefa("teste", "teste", DateTime.Now, DateTime.Now, 1);
-        // var tarefa = await _armazenadorTarefa.ArmazenarTarefa(tarefinha);
-        return Ok("Tarefa criada!");
+        var retorno = await _armazenadorTarefa.ArmazenarTarefa(armazenadorTarefaDto);
+        if(retorno is null)
+            return BadRequest();
+
+        return Created("", retorno);
     }
     
+    /// <summary>
+    /// Obtem as tarefas do usuário por seu id
+    /// </summary>
+    /// <param name="userId">Id do usuário responsável pelas tarefas</param>
+    /// <returns>As tarefas do usuário solicitado</returns>
     [HttpGet("{userId}")]
-    [ProducesResponseType(typeof(IActionResult), StatusCodes.Status200OK)]
-    public async Task<IActionResult> ObterTarefa([FromRoute] string userId)
+    [ProducesResponseType(typeof(IEnumerable<TarefaDto>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(IActionResult), StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> ObterTarefa([FromRoute] int userId)
     {
-        return Ok($"Tarefas do {userId} sendo listadas");
+        var resultado = await _consultaTarefa.ListarTarefasPorUsuarioId(userId);
+        return Ok(resultado);
     }
     
-    [HttpPut("{id}")]
+    /// <summary>
+    /// Altera o status da tarefa para Concluída
+    /// </summary>
+    /// <param name="id">Id da tarefa para realizar sua conclusão</param>
+    [HttpPut("{id}/complete")]
     [ProducesResponseType(typeof(IActionResult), StatusCodes.Status200OK)]
-    public async Task<IActionResult> ConcluirTarefa([FromRoute] string id)
+    [ProducesResponseType(typeof(IActionResult), StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> ConcluirTarefa([FromRoute] int id)
     {
-        return Ok($"Tarefa {id} sendo concluída");
+        await _concluidorTarefa.ConcluirTarefa(id);
+        return Ok();
     }
     
+    /// <summary>
+    /// Remove a tarefa solicitada
+    /// </summary>
+    /// <param name="id">Id da tarefa para ser removida</param>
     [HttpDelete("{id}")]
     [ProducesResponseType(typeof(IActionResult), StatusCodes.Status200OK)]
-    public async Task<IActionResult> ExcluirTarefa([FromRoute] string id)
+    [ProducesResponseType(typeof(IActionResult), StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> ExcluirTarefa([FromRoute] int id)
     {
-        return Ok($"Tarefa {id} sendo concluída");
+        await _removedorTarefa.ExcluirTarefaPorId(id);
+        return Ok();
     }
     
 }
