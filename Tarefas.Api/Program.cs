84c5b4cea9ec4d1395106fda84024c3e33af895e
Andre Furlan <furlan.andre91@gmail.com>
2025-04-22 22:42:24 -0400

    feat: Adicionado banco de dados InMemory SQLite, retorno personalizado de exceções para deixar mais amigável e exportação da documentação para o swagger

--- Tarefas.Api/Program.cs
+++ Tarefas.Api/Program.cs
@@ -1,16 +1,74 @@
+using System.Reflection;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Tarefas.Aplicacao.InjecaoDependencias;
+using Tarefas.Infra.Configurations;
+using Tarefas.Infra.Database;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AdicionarInjecaoInfra();
+builder.Services.AdicionarInjecaoDependenciasAplicacao();
+
+var connection = new SqliteConnection("DataSource=:memory:");
+connection.Open();
+builder.Services.AddDbContext<BancoDadosContexto>(options =>
+    options.UseSqlite(connection));
 
 builder.Services.AddControllers();
 
+builder.Services.AddSwaggerGen(c =>
+{
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
+
 var app = builder.Build();
+
+app.UseExceptionHandler(exceptionHandlerApp =>
+{
+    exceptionHandlerApp.Run(async contexto =>
+    {
+        contexto.Response.ContentType = "application/json";
+        var excecao = contexto.Features.Get<IExceptionHandlerFeature>()?.Error;
+        if (excecao != null)
+        {
+            var statusCode = excecao switch
+            {
+                ArgumentException => StatusCodes.Status400BadRequest,  
+                KeyNotFoundException => StatusCodes.Status404NotFound,
+                _ => StatusCodes.Status500InternalServerError         
+            };
+
+            contexto.Response.StatusCode = statusCode;
+
+            var resposta = new
+            {
+                status = statusCode,
+                mensagem = excecao.Message
+            };
+
+            await contexto.Response.WriteAsJsonAsync(resposta);
+        }
+    });
+});
+
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<BancoDadosContexto>();
+    db.Database.EnsureCreated();
+}
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+
 }
 app.MapControllers();
 app.Run();
--- Tarefas.Infra/Configurations/InfraConfigurations.cs
+++ Tarefas.Infra/Configurations/InfraConfigurations.cs
@@ -1,21 +1,14 @@
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Tarefas.Aplicacao.Tarefas;
-using Tarefas.Infra.Database;
 using Tarefas.Infra.Tarefas;
 
 namespace Tarefas.Infra.Configurations;
 
 public static class InfraConfigurations
 {
-    public static IServiceCollection AdicionarInfra(this IServiceCollection services)
+    public static IServiceCollection AdicionarInjecaoInfra(this IServiceCollection services)
     {
-        services.AddDbContext<BancoDadosContexto>(options =>
-        {
-            options.UseSqlite("DataSource=:memory:");
-        });
-        
+
         services.AddScoped<ITarefaRepository, TarefaRepository>();
 
         return services;
